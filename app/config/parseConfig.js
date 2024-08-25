97f64816a3b9a4774ecf38ff699a9a2e849ca78f
Aliac <alimehdizadeh822@gmail.com>
2024-08-25 21:46:31 +0330

    fix: removed unnecessary cloud env variable and option from config and docker-compose

--- app/config/parseConfig.js
+++ app/config/parseConfig.js
@@ -6,6 +6,5 @@ module.exports = {
   appId: process.env.PARSE_SERVER_APPLICATION_ID || "myAppId",
   masterKey: process.env.PARSE_SERVER_MASTER_KEY || "myMasterKey", // Keep it secret!
   serverURL: process.env.PARSE_SERVER_URL || "http://localhost:1337/parse", // Default to local URL if env variable is not set
-  // cloud: path.join(__dirname, "../../cloud/main.js"),
   masterKeyIps: ["0.0.0.0/0", "::1", "::ffff:172.21.0.1"], // insecure for production
 };
